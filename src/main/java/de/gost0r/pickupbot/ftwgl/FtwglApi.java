429e1c93106ddfeb7d0fc37ecd69f3caaad4a6b3
Holycrap <holycrapurt@gmail.com>
2025-07-21 15:21:35 -0400

    Fixed missing import

--- src/main/java/de/gost0r/pickupbot/ftwgl/FtwglApi.java
+++ src/main/java/de/gost0r/pickupbot/ftwgl/FtwglApi.java
@@ -24,6 +24,7 @@ import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
